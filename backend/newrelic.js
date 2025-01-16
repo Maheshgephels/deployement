64542145d2d3a1c4daf0e18af38c73426fc29e9d
Maheshgephels <mahesh.gephels@gmail.com>
2025-01-16 12:55:49 +0530

    rr
    rtr

--- backend/newrelic.js
+++ backend/newrelic.js
@@ -4,6 +4,6 @@ exports.config = {
     app_name: ['Consoft Application'], // Replace with your application's name
     license_key: '97a358c5fe9eec7cfe35f04663d2e840FFFFNRAL', // Replace with your New Relic license key
     logging: {
-        level: 'info',
+        level: 'trace', // Logging level: 'trace', 'debug', 'info', 'warn', 'error', or 'fatal'
     },
 };
